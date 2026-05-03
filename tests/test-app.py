fecd3e265567762f3d45cabf5e1fb623d42e2c4e
maa0703 <maa152937@gmail.com>
2026-05-03 17:51:55 +0200

    cicd-pipeline

---
+++ tests/test-app.py
@@ -0,0 +1,25 @@
+
+import sys
+import os
+
+# Dit à Python où trouver app.py
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'app'))
+
+from app import app
+import pytest
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    r = client.get("/")
+    assert r.status_code == 200
+    assert b"Hello CI/CD" in r.data
+
+def test_health(client):
+    r = client.get("/health")
+    assert r.status_code == 200
+    assert b"healthy" in r.data
