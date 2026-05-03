e41b71f7916e7c2ed319ee5238511897a41cd491
maa0703 <maa152937@gmail.com>
2026-05-03 20:47:54 +0200

    Test3

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello CI/CD ", "status": "ok"})
+    return jsonify({"message": "Hello CI-CD ", "status": "ok"})
 
 @app.route("/health")
 def health():
